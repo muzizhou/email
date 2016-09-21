d12d34f77f9fec5e8ca60800f5e466dab1f53f02
muzizhou <muzizhou94@163.com>
2016-09-22 00:27:02 +0800

    wangyi

---
+++ js/wang.js
@@ -0,0 +1,7 @@
+$(function(){
+	$("#fullpage").fullpage({
+		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+		navigation:true,
+		navigationTooltips:["首页","视觉","交互","皮肤","功能","代办邮件","联系人邮件","科技","链接易信","马上体验"]
+	});
+});
